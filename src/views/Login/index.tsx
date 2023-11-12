af9329c408591c4c3ae83d4c27844b80965c176f
wiaan <wiaan.botha@spandigital.com>
2023-11-12 12:47:38 +0200

    readded the loading I accidentally got rid of

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,16 +1,20 @@
+import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { pick } from 'lodash'
 
 const Login = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   function handleMe() {
+    setLoading(true);
     axios({
       method: 'get',
       url: 'http://localhost:8080/me',
     }).then(async (res) => {
       localStorage.setItem('user', JSON.stringify(pick(res.data, ['id', 'display_name', 'country', 'product', 'images'])));
+      setLoading(false);
       navigate('/')
     });
   }
@@ -22,7 +26,9 @@ const Login = () => {
           <h2 className="card-title">Welcome to Spotlight</h2>
           <p>Log in to get going!</p>
           <div className="card-actions justify-end">
-            <button className="btn bg-green-400 text-black hover:text-white" onClick={handleMe}>Login</button>
+            <button className="btn bg-green-400 text-black hover:text-white w-24" onClick={handleMe}>
+              { loading ? <span className="loading loading-spinner"></span> : 'Login'}
+            </button>
           </div>
         </div>
       </div>
