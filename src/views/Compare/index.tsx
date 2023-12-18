82b01f7ba34607573c950a5ba8acd02f4d5ce051
Marie <jeannemariebuckle@gmail.com>
2023-12-18 12:09:13 +0200

    Disabled text selection

--- src/views/Compare/index.tsx
+++ src/views/Compare/index.tsx
@@ -46,7 +46,7 @@ const Compare: React.FC = () => {
                 <button className="btn text-white" disabled={step == 0} onClick={() => handleStep(-1)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7" /></svg>
                 </button>
-                <ul className="steps w-[40vw] my-[10vh]">
+                <ul className="steps w-[40vw] my-[10vh] select-none">
                     {stepOptions.map((option, index) => {
                         return <li key={option} className={`step ${index <= step ? 'step-primary' : ''}`}>{option}</li>
                     })}
