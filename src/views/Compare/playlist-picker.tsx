879244985d68c8ec45631eab29824311a19a9f23
wiaan <wiaan.botha@spandigital.com>
2024-01-15 21:44:25 +0200

    formatting changes

--- src/views/Compare/playlist-picker.tsx
+++ src/views/Compare/playlist-picker.tsx
@@ -37,7 +37,8 @@ const PlaylistPicker: React.FC<IPlaylistPickerProps> = ({ playlists, onChange })
                     <div className="text-sm text-gray-500">{playlist.tracks} tracks</div>
                 </div>
                 <button className="btn btn-square btn-outline btn-sm text-red-400" onClick={() => onChange(playlists.filter(p => p.id !== playlist.id))}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>                </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </div>
         )
     }
